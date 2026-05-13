be3d9d1d465dffdf83057a2c2d0725897d987513
achaval-tomas <tomiachaval2013@gmail.com>
2026-05-13 20:10:30 -0300

    variable rename in encoder driver

--- Variometer/Drivers/encoder/encoder.c
+++ Variometer/Drivers/encoder/encoder.c
@@ -4,60 +4,60 @@
 #include "stm32h5xx_hal.h"
 
 // Handle para el software timer que revisará al clk del encoder
-static TimerHandle_t encoder_periodic_timer = NULL;
+static TimerHandle_t encoder_PeriodicTimer = NULL;
 
 // Handle para el one-shot timer de los long-press
-static TimerHandle_t encoder_long_press_timer = NULL;
+static TimerHandle_t encoder_LongPressTimer = NULL;
 
 // Variables de estado privadas
-static TIM_HandleTypeDef *encoder_htim;
-static QueueHandle_t encoder_queue;
-static volatile uint16_t lastTimerCount = 0;
+static TIM_HandleTypeDef *encoder_TIM;
+static QueueHandle_t encoder_Queue;
+static volatile uint16_t encoder_LastTIMCount = 0;
 
-static volatile uint32_t buttonPressTime = 0;
-static volatile bool buttonIsPressed = false;
+static volatile uint32_t encoder_ButtonPressTime = 0;
+static volatile bool encoder_ButtonIsPressed = false;
 
 // Trigger periodicamente cada RE_POLL_INTERVAL_MS para revisar el contador del timer
 static void RE_PeriodicTimerCallback(TimerHandle_t xTimer) {
-    uint16_t currentCount = __HAL_TIM_GET_COUNTER(encoder_htim);
-    int16_t delta = (int16_t)(currentCount - lastTimerCount);
-
+    uint16_t currentCount = __HAL_TIM_GET_COUNTER(encoder_TIM);
+    int16_t delta = (int16_t)(currentCount - encoder_LastTIMCount);
+    
     // El timer se mueve 2 pasos por cada paso de rotacion fisico
     int16_t steps = delta / 2;
-
+    
     if (steps != 0) {
-        encoderEvent_td newEvent;
-        newEvent.type = ENCODER_EVENT_ROTATION;
-        newEvent.delta = steps;
-        
-        lastTimerCount += (steps * 2); 
-        
-        // Encolar la rotación, non-blocking
-        xQueueSend(encoder_queue, &newEvent, 0); 
-    }
+        // Encolar el evento de rotación, non-blocking
+        encoderEvent_td newEvent = {ENCODER_EVENT_ROTATION, steps};
+        xQueueSend(encoder_Queue, &newEvent, 0);
+
+        // Actualizar LastTIMCount sumando pasos ya procesados
+        encoder_LastTIMCount += steps*2;
+    } 
 }
 
 // Trigger cuando el botón se mantiene apretado más de RE_LONG_PRESS_MS
 static void RE_LongPressTimerCallback(TimerHandle_t xTimer){
-    if (encoder_queue == NULL) return;
-    if (!buttonIsPressed) return;
-    buttonIsPressed = false;
+    if (encoder_Queue == NULL) return;
 
+    // Si es falso, ya lo manejo el Rising Callback por un click corto
+    if (!encoder_ButtonIsPressed) return;
+
+    encoder_ButtonIsPressed = false;
     encoderEvent_td newEvent = {ENCODER_EVENT_LONG_PRESS, 0};
 
     // Encolar el long press, non-blocking
-    xQueueSend(encoder_queue, &newEvent, 0); 
+    xQueueSend(encoder_Queue, &newEvent, 0); 
 }
 
 void RE_Init(TIM_HandleTypeDef *htim, QueueHandle_t eventsQueue) {
-    encoder_htim = htim;
-    HAL_TIM_Encoder_Start(encoder_htim, TIM_CHANNEL_ALL);
-    lastTimerCount = __HAL_TIM_GET_COUNTER(encoder_htim);
+    encoder_TIM = htim;
+    HAL_TIM_Encoder_Start(encoder_TIM, TIM_CHANNEL_ALL);
+    encoder_LastTIMCount = __HAL_TIM_GET_COUNTER(encoder_TIM);
 
-    encoder_queue = eventsQueue;
+    encoder_Queue = eventsQueue;
 
     // Software timer periodico para pollear el contador del HW timer
-    encoder_periodic_timer = xTimerCreate(
+    encoder_PeriodicTimer = xTimerCreate(
         "rotations Timer", 
         pdMS_TO_TICKS(RE_POLL_INTERVAL_MS), 
         pdTRUE, // autoReload
@@ -66,7 +66,7 @@ void RE_Init(TIM_HandleTypeDef *htim, QueueHandle_t eventsQueue) {
     );
 
     // One-shot timer para detectar long-press del pulsador
-    encoder_long_press_timer = xTimerCreate(
+    encoder_LongPressTimer = xTimerCreate(
         "long press Timer", 
         pdMS_TO_TICKS(RE_LONG_PRESS_MS), 
         pdFALSE, // one-shot
@@ -78,53 +78,52 @@ void RE_Init(TIM_HandleTypeDef *htim, QueueHandle_t eventsQueue) {
 }
 
 void RE_Enable_Rotations(){
-    if (encoder_periodic_timer == NULL) return;
+    if (encoder_PeriodicTimer == NULL) return;
 
     // Start the software timer
-    xTimerReset(encoder_periodic_timer, 0);
-    lastTimerCount = __HAL_TIM_GET_COUNTER(encoder_htim);
+    xTimerReset(encoder_PeriodicTimer, 0);
+    encoder_LastTIMCount = __HAL_TIM_GET_COUNTER(encoder_TIM);
 }
 
 void RE_Disable_Rotations(){
-    if (encoder_periodic_timer == NULL) return;
+    if (encoder_PeriodicTimer == NULL) return;
 
-    xTimerStop(encoder_periodic_timer, 0);
+    xTimerStop(encoder_PeriodicTimer, 0);
 }
 
 // FALLING = PRESIONADO
 void RE_EXTI_Falling_Callback(uint16_t GPIO_Pin) {
     if (GPIO_Pin != ENCODER_SW_PIN) return; 
+    // Ignorar multiples triggers si aun no fue procesado el primero
+    if (encoder_ButtonIsPressed) return;
 
-    if (!buttonIsPressed) {
-        buttonPressTime = HAL_GetTick();
-        buttonIsPressed = true;
+    encoder_ButtonPressTime = HAL_GetTick();
+    encoder_ButtonIsPressed = true;
 
-        // Iniciar one-shot timer para detectar long-press
-        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        xTimerStartFromISR(encoder_long_press_timer, &xHigherPriorityTaskWoken);
-        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-    }
-
+    // Iniciar one-shot timer para detectar long-press
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xTimerStartFromISR(encoder_LongPressTimer, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 // RISING = LIBERADO
 void RE_EXTI_Rising_Callback(uint16_t GPIO_Pin) {
-    if (GPIO_Pin != ENCODER_SW_PIN) return;
-    if (encoder_queue == NULL) return;
+    if (GPIO_Pin != ENCODER_SW_PIN || encoder_Queue == NULL) return;
     
     // Si es falso, ya lo manejó el one-shot timer por long-press
-    if (!buttonIsPressed) return;
+    if (!encoder_ButtonIsPressed) return;
     
     // Matar el one-shot timer que detectaría un long-press
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xTimerStopFromISR(encoder_long_press_timer, &xHigherPriorityTaskWoken);
+    xTimerStopFromISR(encoder_LongPressTimer, &xHigherPriorityTaskWoken);
     
-    buttonIsPressed = false;
+    encoder_ButtonIsPressed = false;
     
     // Solo encolar eventos de clicks >= RE_CLICK_MS
-    if ((HAL_GetTick() - buttonPressTime) >= RE_CLICK_MS){
+    if ((HAL_GetTick() - encoder_ButtonPressTime) >= RE_CLICK_MS){
+        // Sabemos que RE_CLICK_MS <= duración del click < RE_LONG_PRESS_MS
         encoderEvent_td newEvent = {ENCODER_EVENT_CLICK, 0};
-        xQueueSendFromISR(encoder_queue, &newEvent, &xHigherPriorityTaskWoken);
+        xQueueSendFromISR(encoder_Queue, &newEvent, &xHigherPriorityTaskWoken);
     }
 
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
