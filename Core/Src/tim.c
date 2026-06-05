f68152c3918606019d27c3eb83901ea8aa1b9ce7
achaval-tomas <tomiachaval2013@gmail.com>
2026-06-05 18:50:56 -0300

    config timer for new buzzer module with 5v logic

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -186,7 +186,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PA0     ------> TIM2_CH1
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
